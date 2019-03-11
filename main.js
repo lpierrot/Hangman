64a39db792efb495e19939bd96160b2e078a3101
lpierrot <pierrotlione@seattleu.edu>
2019-03-10 21:18:21 -0700

    fixing JS

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var currentWord = 'hello';
+    var possibleWords =[
+        {"word": "Washington", "hint": "Place"},
+        { "word": "Obama", "hint": "Person" },
+        { "word": "Volvo", "hint": "Car Maker" },
+        { "word": "Pizza", "hint": "Food" },
+        {
+            "word": "Seattle",
+            "hint": "City"
+        }
+    ];
+    var word = possibleWords[Math.floor(Math.random() * Math.floor(possibleWords.length))];
+    var currentWord = word['word'];
+    var hint = word['hint'];
+    var hintContainer = document.getElementById('hint');
+    hintContainer.textContent = hint;
     var numberBox = currentWord.length;
+    var numberOfGuesses= 6;
+    var previousGuesses = { }
     var wordContainer = document.getElementById('words');
     var div = document.createElement('div');
     wordContainer.append(div);
@@ -11,15 +27,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     document.onkeypress = function(e) {
         console.log(e);
-        if (currentWord.indexOf(e.key) >= 0) {
-            var lastPosition = 0;
-            while (i <document.indexOf(e.key) ) {
-                console.log(`This is loop iteration number ${i}`);
-                i++;
-            var position = currentWord.indexOf(e.key);
-            var block = document.getElementById(position.toString());
-            block.textContent = e.key;
-        } else {}
+        if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode > 96 && e.keyCode <123)){
+          
+            if (!(e.key.toLowerCase() in previousGuesses)) {
+                previousGuesses[e.key.toLowerCase()] = true;
+                var guesses = document.getElementById('guesses');
+                guesses.textContent += " " + e.key.toLowerCase();
+                if (currentWord.toLowerCase().indexOf(e.key.toLowerCase()) >= 0) {
+                    var lastPosition = -1;
+                    while (currentWord.toLowerCase().indexOf(e.key.toLowerCase(), lastPosition + 1) >= 0) {
+                        lastPosition = currentWord.toLowerCase().indexOf(e.key.toLowerCase(), lastPosition + 1);
+                        var block = document.getElementById(lastPosition.toString());
+                        numberBox -= 1;
+                        block.textContent = currentWord[lastPosition];
+                    }
+                } else { 
+                    numberOfGuesses -= 1;
+                    if (numberOfGuesses === 0) {
+                        var feedback = document.getElementById('feedback');
+                        feedback.textContent = 'You lose';
+                    }
+                }
+
+            }
+            if (numberBox===0) {
+                var feedback = document.getElementById('feedback');
+                feedback.textContent = 'Good Job';
+            }
+
+
+        }
+
     } 
 
 }
