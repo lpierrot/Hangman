8d4dab6d9ef1c5242c3bdf271886632f69c15dc9
lpierrot <pierrotlione@seattleu.edu>
2019-03-13 12:19:08 -0700

    changed project to image slide

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hangman
+# Image Slider
--- main.js
+++ main.js
@@ -1,65 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var possibleWords =[
-        {"word": "Washington", "hint": "Place"},
-        { "word": "Obama", "hint": "Person" },
-        { "word": "Volvo", "hint": "Car Maker" },
-        { "word": "Pizza", "hint": "Food" },
-        {
-            "word": "Seattle",
-            "hint": "City"
-        }
-    ];
-    var word = possibleWords[Math.floor(Math.random() * Math.floor(possibleWords.length))];
-    var currentWord = word['word'];
-    var hint = word['hint'];
-    var hintContainer = document.getElementById('hint');
-    hintContainer.textContent = hint;
-    var numberBox = currentWord.length;
-    var numberOfGuesses= 6;
-    var previousGuesses = { }
-    var wordContainer = document.getElementById('words');
-    var div = document.createElement('div');
-    wordContainer.append(div);
-    for (var i=0; i<numberBox; i++) {
-        var div = document.createElement('div');
-        div.id = i; 
-        wordContainer.append(div);
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+const container = document.querySelector('.images');
+
+let counter = 0;
+
+nextBtn.addEventListener('click',NextSlide);
+prevBtn.addEventListener('click',prevSlide);
+
+function nextSlide(){
+    container.animate([{opacity:'0.1'},{opacity:'1.0'}],{duration:1000, fill:'forwards'});
+    if(counter ===4){
+        counter = -1;
     }
-    document.onkeypress = function(e) {
-        console.log(e);
-        if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode > 96 && e.keyCode <123)){
-          
-            if (!(e.key.toLowerCase() in previousGuesses)) {
-                previousGuesses[e.key.toLowerCase()] = true;
-                var guesses = document.getElementById('guesses');
-                guesses.textContent += " " + e.key.toLowerCase();
-                if (currentWord.toLowerCase().indexOf(e.key.toLowerCase()) >= 0) {
-                    var lastPosition = -1;
-                    while (currentWord.toLowerCase().indexOf(e.key.toLowerCase(), lastPosition + 1) >= 0) {
-                        lastPosition = currentWord.toLowerCase().indexOf(e.key.toLowerCase(), lastPosition + 1);
-                        var block = document.getElementById(lastPosition.toString());
-                        numberBox -= 1;
-                        block.textContent = currentWord[lastPosition];
-                    }
-                } else { 
-                    numberOfGuesses -= 1;
-                    if (numberOfGuesses === 0) {
-                        var feedback = document.getElementById('feedback');
-                        feedback.textContent = 'You lose';
-                    }
-                }
-
-            }
-            if (numberBox===0) {
-                var feedback = document.getElementById('feedback');
-                feedback.textContent = 'Good Job';
-            }
-
-
-        }
-
-    } 
-
+    counter++;
+    container.style.backgroundImage = `url(images/bcg-.${counter}.jpeg)`
 }
-)
 
+function prevSlide() {
+    container.animate([{ opacity: '0.1' }, { opacity: '1.0' }], { duration: 1000, fill: 'forwards' });
+    if (counter === 0) {
+        counter = 5;
+    }
+    counter--;
+    container.style.backgroundImage = `url(img/pic.${counter}.jpg)`
+}
