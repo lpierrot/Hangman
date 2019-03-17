8edbd9e70fce2b1a6cd254655bb8c6c464a21eb8
lpierrot <pierrotlione@seattleu.edu>
2019-03-16 20:24:26 -0700

    completed assignement

--- main.js
+++ main.js
@@ -3,15 +3,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const prevBtn = document.querySelector('.prevBtn');
     const container = document.querySelector('.images');
 
-    let imagePath = `images/bcg-0.jpeg`;
+    const imagePathBegin = 'images/bcg-';
+    const imagePathEnd = '.jpeg';
+    let currentImage = 0;
+    
+    //full = images/bcg-0.jpeg, images/bcg-1.jpeg
+    //image
+
     let imageAlt = 'food';
     let imageEl = document.createElement('img');
-    imageEl.setAttribute("src", imagePath);
+    imageEl.setAttribute("src", imagePathBegin + currentImage + imagePathEnd );
     imageEl.setAttribute("alt", imageAlt);
     container.appendChild(imageEl);
     
+    const nextSlide = (e) => {
+        currentImage += 1;
+        if (currentImage>4) {
+            currentImage=0;
+        }
+        imageEl.setAttribute("src" , imagePathBegin + currentImage + imagePathEnd);
+    }
 
-    nextBtn.addEventListener('click', NextSlide);
+    const prevSlide = (e) => {
+        currentImage -= 1;
+        if (currentImage < 0) {
+            currentImage = 4;
+        }
+        imageEl.setAttribute("src", imagePathBegin + currentImage + imagePathEnd);
+    }
+
+
+    nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
 });
 
